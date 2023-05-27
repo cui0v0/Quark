ab72fd0925a55ae987bfa46351688829287a871f
Vasco Lavos <vazkii@hotmail.com>
2023-05-27 14:04:54 +0100

    button... 2!!!!

---
+++ src/main/java/vazkii/quark/base/client/util/Button2.java
@@ -0,0 +1,14 @@
+package vazkii.quark.base.client.util;
+
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.network.chat.Component;
+
+// Button... TWO!!!!!!!!!
+public class Button2 extends Button {
+
+	// lmao
+	public Button2(int p_259075_, int p_259271_, int p_260232_, int p_260028_, Component p_259351_, OnPress p_260152_) {
+		super(p_259075_, p_259271_, p_260232_, p_260028_, p_259351_, p_260152_, DEFAULT_NARRATION);
+	}
+
+}
