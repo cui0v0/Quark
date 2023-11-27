e380df77a896a486036a0ecb7e159229800c400d
IThundxr <contact@ithundxr.dev>
2023-11-26 20:27:28 -0500

    fix saplings (maybe)

--- src/main/java/org/violetmoon/quark/content/world/block/BlossomSaplingBlock.java
+++ src/main/java/org/violetmoon/quark/content/world/block/BlossomSaplingBlock.java
@@ -1,7 +1,6 @@
 package org.violetmoon.quark.content.world.block;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.ConstantInt;
@@ -9,7 +8,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FancyFoliagePlacer;
@@ -24,10 +22,12 @@ import org.violetmoon.zeta.module.ZetaModule;
 import java.util.OptionalInt;
 
 public class BlossomSaplingBlock extends ZetaSaplingBlock {
+	static String colorName;
 
 	public BlossomSaplingBlock(String colorName, ZetaModule module, BlossomTree tree) {
 		super(colorName + "_blossom", module, tree);
 		tree.sapling = this;
+		BlossomSaplingBlock.colorName = colorName;
 	}
 
 	public static class BlossomTree extends AbstractTreeGrower {
@@ -49,16 +49,38 @@ public class BlossomSaplingBlock extends ZetaSaplingBlock {
 			leaf = leafBlock.defaultBlockState();
 		}
 
-		ResourceKey<ConfiguredFeature<?, ?>> BLOSSOM_TREE = ResourceKey.create(Registries.CONFIGURED_FEATURE, Quark.asResource("blossom_tree"));
+		public static final ResourceKey<ConfiguredFeature<?, ?>> BLUE_BLOSSOM_KEY = registerKey("blue_blossom");
+		public static final ResourceKey<ConfiguredFeature<?, ?>> LAVENDER_BLOSSOM_KEY = registerKey("lavender_blossom");
+		public static final ResourceKey<ConfiguredFeature<?, ?>> ORANGE_BLOSSOM_KEY = registerKey("orange_blossom");
+		public static final ResourceKey<ConfiguredFeature<?, ?>> YELLOW_BLOSSOM_KEY = registerKey("yellow_blossom");
+		public static final ResourceKey<ConfiguredFeature<?, ?>> RED_BLOSSOM_KEY = registerKey("red_blossom");
 
 		@Override
 		protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource rand, boolean hasFlowers) {
-			return BLOSSOM_TREE;
+			ResourceKey<ConfiguredFeature<?, ?>> blossomType;
+
+			switch (colorName) {
+				case "blue" -> blossomType = BLUE_BLOSSOM_KEY;
+				case "lavender" -> blossomType = LAVENDER_BLOSSOM_KEY;
+				case "orange" -> blossomType = ORANGE_BLOSSOM_KEY;
+				case "yellow" -> blossomType = YELLOW_BLOSSOM_KEY;
+				case "red" -> blossomType = RED_BLOSSOM_KEY;
+				default -> {
+					return null;
+				}
+
+			}
+
+			return blossomType;
 		}
 
 		//fixme find how to call this
-		public void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
-			context.register(BLOSSOM_TREE, new ConfiguredFeature<>(Feature.TREE, config));
+//		public void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
+//			context.register(BLOSSOM_TREE, new ConfiguredFeature<>(Feature.TREE, config));
+//		}
+
+		public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+			return ResourceKey.create(Registries.CONFIGURED_FEATURE, Quark.asResource(name));
 		}
 	}
 
