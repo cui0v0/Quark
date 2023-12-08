0cd77fe59b0bd1e3268ae6b6ab496e72eaa17d7b
IThundxr <contact@ithundxr.dev>
2023-12-07 22:13:43 -0500

    feat: ZEarlyRender Event

--- src/main/java/org/violetmoon/zeta/client/event/play/ZRenderTick.java
+++ src/main/java/org/violetmoon/zeta/client/event/play/ZRenderTick.java
@@ -2,6 +2,12 @@ package org.violetmoon.zeta.client.event.play;
 
 import org.violetmoon.zeta.event.bus.IZetaPlayEvent;
 
+/**
+ * <p>
+ * <b>Notice:</b> Using {@link net.minecraft.client.gui.GuiGraphics in this event
+ * will not work, Use {@link ZEarlyRender } instead.
+ * </p>
+ */
 public interface ZRenderTick extends IZetaPlayEvent {
 	float getRenderTickTime();
 	boolean isEndPhase();
