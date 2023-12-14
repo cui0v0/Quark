2681985af482fd43e4dab4fe4b270c16183a38bf
quat1024 <quat1024@users.noreply.github.com>
2023-12-13 22:39:30 -0500

    Just implement ArmedArmorStandsModule with a mixin. Fixes #4470

--- src/main/java/org/violetmoon/quark/content/tweaks/module/ArmedArmorStandsModule.java
+++ src/main/java/org/violetmoon/quark/content/tweaks/module/ArmedArmorStandsModule.java
@@ -1,42 +1,31 @@
 package org.violetmoon.quark.content.tweaks.module;
 
-import org.violetmoon.zeta.event.bus.PlayEvent;
-import org.violetmoon.zeta.event.play.entity.ZEntityConstruct;
+import org.violetmoon.zeta.event.bus.LoadEvent;
+import org.violetmoon.zeta.event.load.ZConfigChanged;
 import org.violetmoon.zeta.module.ZetaLoadModule;
 import org.violetmoon.zeta.module.ZetaModule;
 import org.violetmoon.zeta.util.Hint;
 
-import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
 /**
  * @author WireSegal
  * Created at 8:40 AM on 8/27/19.
+ *
+ * modified by quat
+ * @see org.violetmoon.quark.mixin.ArmorStandMixin
  */
 @ZetaLoadModule(category = "tweaks")
 public class ArmedArmorStandsModule extends ZetaModule {
 
 	@Hint Item armor_stand = Items.ARMOR_STAND;
 
-	@PlayEvent
-	public void entityConstruct(ZEntityConstruct event) {
-		if(event.getEntity() instanceof ArmorStand stand) {
-			if(!stand.isShowArms())
-				setShowArms(stand, true);
-		}
+	public static boolean staticEnabled = true;
+
+	@LoadEvent
+	public void configChange(ZConfigChanged e) {
+		staticEnabled = enabled;
 	}
 
-	private void setShowArms(ArmorStand e, boolean showArms) {
-		e.getEntityData().set(ArmorStand.DATA_CLIENT_FLAGS, setBit(e.getEntityData().get(ArmorStand.DATA_CLIENT_FLAGS), 4, showArms));
-	}
-
-	private byte setBit(byte status, int bitFlag, boolean value) {
-		if (value)
-			status = (byte)(status | bitFlag);
-		else
-			status = (byte)(status & ~bitFlag);
-
-		return status;
-	}
 }
