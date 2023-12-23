b7915381c606263c4aa67e952798f030f948baa2
yrsegal@gmail.com <yrsegal@gmail.com>
2023-12-23 13:00:16 -0500

    for some reason this hint was acting weird

--- src/main/java/org/violetmoon/quark/content/tools/module/ColorRunesModule.java
+++ src/main/java/org/violetmoon/quark/content/tools/module/ColorRunesModule.java
@@ -60,7 +60,7 @@ public class ColorRunesModule extends ZetaModule {
 	public static final String TAG_RUNE_COLOR = Quark.MOD_ID + ":RuneColor";
 
 	private static final ThreadLocal<RuneColor> targetColor = new ThreadLocal<>();
-	@Hint
+	@Hint(key = "smithing_template_rune")
 	public static Item rune;
 
 	@Config
